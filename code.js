67fdf37bfa8f23d9644bd9649e64fc8ef35e6985
Pardis <pardisghadrdan@gmail.com>
2018-03-13 11:12:40 +0100

    2-8 stars stars rating added

--- code.js
+++ code.js
@@ -11,5 +11,26 @@ function renderMovie(movie){
     document.querySelector(".infosection ul").innerHTML = actorList;
 
 }
+function changeStarRating(rating){
+  for (let i = 1; i < 6; i++) {
+    let star = document.getElementById("star" + i);
+    if (i <= rating){
+      star.classList.add("filled");
+    } else {
+      star.classList.remove("filled");
+    }
+  }
+}
+
+//Eventhanterare
+for(let i=1; i<=5; i++){
+  let star = document.getElementById("star" + i);
+  star.addEventListener("click", function(){
+    changeStarRating(i);
+  });
+}
+
+
+
 
 renderMovie(movieData);
